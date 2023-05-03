78ec4810f4f5c728f2780b85dd326c119e308b72
sourav4125 <souravmisra4125@gmail.com>
2023-05-03 16:39:27 +0530

    quiz

---
+++ src/Data/quizdata.js
@@ -0,0 +1,28 @@
+export const QuizData = [
+    {
+        question: "Javascript is an _______ language?",
+        options:[ "Object-Oriented","Object-Based","Procedural","None of the above" ],
+        answer: 1
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        options:["var", "let", "Both A & B", "None of the above"],
+        answer: 3
+    },
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        options: ["getElementbyId()", "getElementsByClassName()", "Both A & B", "None of the above"],
+        answer: 3
+    },
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        options: ["document.write()", "console.log()", "window.alert()", "all of the above"],
+        answer:4
+    },
+    {
+        question: "How can a datatype be declared to be a constant type?",
+        options: ["const", "var", "let", "all of the above"],
+        answer:1
+    },
+
+];
